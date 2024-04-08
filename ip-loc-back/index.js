03d436c88997df8da74d35b2c4a6c9f53c75f17e
Jules Royet <jules.royet@orange.com>
2024-04-08 17:38:49 +0200

    :sparkles: Ajout de swagger-ui et complétion du README.md

--- ip-loc-back/index.js
+++ ip-loc-back/index.js
@@ -1,7 +1,30 @@
 require('dotenv').config();
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const app = express();
 const mongoose = require('mongoose');
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8w9vhnm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Options de configuration pour swagger-jsdoc
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'IP Locator',
+      version: '1.0.0',
+      description: 'Documentation de l\'API IP Locator permettant de localiser une adresse IP',
+    },
+  },
+  apis: ['./routes/*.js'], // Emplacement des fichiers contenant les annotations Swagger
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+const port = 8080;
+
+// Route pour servir la documentation Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 async function run() {
   try {
@@ -13,5 +36,10 @@ async function run() {
     await mongoose.disconnect();
   }
 }
+
+
+app.listen(port, () => {
+  console.log(`Serveur démarré sur http://localhost:${port}`);
+});
 
 run().catch(console.error);
